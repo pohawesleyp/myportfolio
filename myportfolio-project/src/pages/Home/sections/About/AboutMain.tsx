bb8326ff38627834fe030ef04d825815898ffe9c
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-07-31 18:13:14 -0300

    perf/docs: name change and code refactoring

--- myportfolio-project/src/pages/Home/sections/About/AboutMain.tsx
+++ myportfolio-project/src/pages/Home/sections/About/AboutMain.tsx
@@ -1,11 +1,11 @@
 import { styled, Grid } from "@mui/material";
-import { Container, Typography } from "@mui/material";
+import { Container } from "@mui/material";
 import { Box } from "@mui/system";
 import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
-import TypewriterBody from "../../../../components/Typewriter/TypewriterBody";
 import AboutHeader from "./AboutHeader";
 import AboutCards from "./AboutCards";
 import AboutDescription from "./AboutDescription";
+import AboutCouses from "./AboutCourses";
 
 const StyledAbout = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.secondary.contrastText,
@@ -13,11 +13,6 @@ const StyledAbout = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   color: theme.palette.primary.contrastText,
-}));
-
-const AboutSection = styled(Box)(({ theme }) => ({
-  marginBottom: theme.spacing(4),
-  textAlign: "left",
 }));
 
 const About: React.FC = () => {
@@ -28,6 +23,7 @@ const About: React.FC = () => {
           <br />
           <AboutHeader />
           <AboutCards />
+
           <Grid container spacing={2}>
             <Grid size={{ xs: 12, md: 5 }}>
               <Box position="relative" pb={3}>
@@ -45,90 +41,7 @@ const About: React.FC = () => {
           </Grid>
 
           <AboutDescription />
-
-          <AboutSection>
-            <Typography
-              variant="h3"
-              gutterBottom
-              fontStyle="italic"
-              fontWeight="bold"
-              textAlign="center"
-            >
-              Courses with Certificates
-            </Typography>
-
-            <TypewriterBody
-              text="        Institution: Ada Tech (Brazil)"
-              delay={40}
-              variant="body1"
-              color="white"
-            ></TypewriterBody>
-            <br />
-            <TypewriterBody
-              text="Course: Front-End Development with a Full Stack Approach"
-              delay={40}
-              variant="body1"
-              color="white"
-            ></TypewriterBody>
-            <br />
-            <TypewriterBody
-              text="Institution: Udemy (Brazil)"
-              delay={40}
-              variant="body1"
-              color="white"
-            ></TypewriterBody>
-            <br />
-            <TypewriterBody
-              text="Course: Web Development Bootcamp Certificate (Java, Python, C, C++
-              and C#)"
-              delay={40}
-              variant="body1"
-              color="white"
-            ></TypewriterBody>
-            <br />
-            <TypewriterBody
-              text=" Institution: Santander Open Academy (Brazil)"
-              delay={40}
-              variant="body1"
-              color="white"
-            ></TypewriterBody>
-            <br />
-            <TypewriterBody
-              text=" Course: Python Basics"
-              delay={40}
-              variant="body1"
-              color="white"
-            ></TypewriterBody>
-            <br />
-            <TypewriterBody
-              text="Institution: Udemy (Brazil)"
-              delay={40}
-              variant="body1"
-              color="white"
-            ></TypewriterBody>
-            <br />
-            <TypewriterBody
-              text="Course: Python Basics"
-              delay={40}
-              variant="body1"
-              color="white"
-            ></TypewriterBody>
-            <br />
-            <TypewriterBody
-              text="Institution: Gustavo Guanabara (Brazil)"
-              delay={40}
-              variant="body1"
-              color="white"
-            ></TypewriterBody>
-            <br />
-            <TypewriterBody
-              text="Course: Web Development Bootcamp Certificate (HTML, CSS and
-              JavaScript)"
-              delay={40}
-              variant="body1"
-              color="white"
-            ></TypewriterBody>
-          </AboutSection>
+          <AboutCouses />
         </Container>
       </StyledAbout>
     </>
