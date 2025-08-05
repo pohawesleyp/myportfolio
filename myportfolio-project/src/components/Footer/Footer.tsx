0cde933c980576bdc43c18218cb8ca7bc2b273d6
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-08-05 17:23:46 -0300

    perf: Changes in the responsiveness of Footer.tsx and added a button with a link to Whatsapp

--- myportfolio-project/src/components/Footer/Footer.tsx
+++ myportfolio-project/src/components/Footer/Footer.tsx
@@ -9,23 +9,24 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import theme from "../../theme";
+import { WhatsApp } from "@mui/icons-material";
+
+const StyledFooter = styled("footer")(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.primary.contrastText,
+  padding: "2rem",
+  textAlign: "center",
+}));
+
+const iconButtonStyle = {
+  transition: "transform 0.3s ease, color 0.3s ease",
+  "&:hover": {
+    transform: "scale(1.5)",
+    color: `${theme.palette.secondary.main}`, // Ou uma cor específica, se quiser dar contraste
+  },
+};
 
 const Footer: React.FC = () => {
-  const StyledFooter = styled("footer")(({ theme }) => ({
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
-    padding: "2rem",
-    textAlign: "center",
-  }));
-
-  const iconButtonStyle = {
-    transition: "transform 0.3s ease, color 0.3s ease",
-    "&:hover": {
-      transform: "scale(1.2)",
-      color: `${theme.palette.secondary.main}`, // Ou uma cor específica, se quiser dar contraste
-    },
-  };
-
   return (
     <>
       <StyledFooter>
@@ -40,7 +41,7 @@ const Footer: React.FC = () => {
             <IconButton
               href="https://github.com/pohawesleyp"
               target="_blank"
-              rel="noopener"
+              rel="noopener noreferrer"
               aria-label="GitHub"
               sx={iconButtonStyle}
             >
@@ -49,7 +50,7 @@ const Footer: React.FC = () => {
             <IconButton
               href="https://www.linkedin.com/in/wesley-amaro/"
               target="_blank"
-              rel="noopener"
+              rel="noopener noreferrer"
               aria-label="Linkedin"
               sx={iconButtonStyle}
             >
@@ -58,17 +59,27 @@ const Footer: React.FC = () => {
             <IconButton
               href="mailto:wesley.amaro.1@gmail.com"
               target="_blank"
-              rel="noopener"
+              rel="noopener noreferrer"
               aria-label="Gmail"
               sx={iconButtonStyle}
             >
               <EmailIcon fontSize="large" />
+            </IconButton>
+            <IconButton
+              href="https://wa.me/5513981726612?text=Olá%20Wesley,%20gostaria%20de%20entrar%20em%20contato%20com%20você!"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="WhatsApp"
+              sx={iconButtonStyle}
+            >
+              <WhatsApp fontSize="large" />
             </IconButton>
           </Stack>
           <Typography
             variant="body2"
             color="primary.contrastText"
             textAlign="center"
+            fontWeight={"bold"}
             sx={{
               fontSize: { xs: "0.75rem", sm: "0.875rem" },
             }}
