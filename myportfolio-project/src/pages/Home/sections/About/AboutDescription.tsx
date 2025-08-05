9b8bebaba25a53096507cc9fa068def20609a37b
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-08-05 17:24:47 -0300

    Code changes for better session performance -About Me-

--- myportfolio-project/src/pages/Home/sections/About/AboutDescription.tsx
+++ myportfolio-project/src/pages/Home/sections/About/AboutDescription.tsx
@@ -1,15 +1,7 @@
 import { styled, Grid } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import { Element } from "react-scroll";
-
-// const StyledAbout = styled("div")(({ theme }) => ({
-//   backgroundColor: theme.palette.secondary.contrastText,
-//   minHeight: "100vh",
-//   display: "flex",
-//   alignItems: "center",
-//   color: theme.palette.primary.contrastText,
-// }));
+// import { Element } from "react-scroll";
 
 const AboutSection = styled(Box)(({ theme }) => ({
   marginBottom: theme.spacing(4),
@@ -19,57 +11,56 @@ const AboutSection = styled(Box)(({ theme }) => ({
 const AboutDescription: React.FC = () => {
   return (
     <>
-      <Element name="about">
-        <AboutSection id="about" tabIndex={-1} aria-labelledby="about-title">
-          <Grid size={{ xs: 12, md: 7 }}>
-            <Typography
-              variant="body2"
-              paragraph
-              fontFamily="serif"
-              fontWeight="bold"
-              fontSize={16}
-              textAlign="center"
-              flexDirection="column"
-            >
-              Prazer, eu sou Wesley Amaro (mas pode me chamar de Wes). Tenho 28
-              anos, moro em São Paulo e sou apaixonado por dar vida a interfaces
-              incríveis. Sou estudante de ADS, dev Front-End por vocação,
-              Full-Stack em evolução e curioso profissional de T.I. há 6 anos.
-              <br />
-              <br />
-              Ao longo da minha trajetória profissional, atuei em áreas como
-              Desenvolvimento, Suporte Técnico, Assistência em T.I., Atendimento
-              e Liderança. Essa bagagem me deu visão ampla, foco em resultados e
-              jogo de cintura pra resolver qualquer pepino.
-              <br />
-              <br />
-              Atualmente finalizando minha graduação em Análise e
-              Desenvolvimento de Sistemas (ADS), com foco em Front-End e
-              Full-Stack. Tenho certificações em tecnologias web como HTML, CSS,
-              JavaScript, TypeScript, Node.js, React, Tailwind, Sass, Material
-              UI e Figma.
-              <br />
-              <br />
-              Também estou expandindo minha base Full Stack com Python e SQL, e
-              faço do GitHub meu companheiro diário. De quebra, tenho noções de
-              linguagens como Java, C, C++ e C#, porque conhecer várias
-              linguagens é sempre poder de fogo extra.
-              <br />
-              <br />
-              São mais de 10 anos de experiência no mercado, 6 deles dedicados à
-              tecnologia. Nesse tempo, contribuí para o crescimento da Niu Fibra
-              (empresa júnior de Guarujá que hoje integra o grupo Alloha Fibra),
-              otimizando processos, criando soluções e melhorando a experiência
-              dos clientes com ferramentas como Excel, PowerPoint, Teams, 3CX,
-              AnyDesk, além de muito suporte a softwares e hardwares internos.
-              <br />
-              <br />
-              Meu objetivo? Seguir evoluindo como desenvolvedor, criando
-              soluções que impactem pessoas e negócios de verdade.
-            </Typography>
-          </Grid>
-        </AboutSection>
-      </Element>
+      {/* <Element name="about"> */}
+      <AboutSection id="" tabIndex={-1} aria-labelledby="about-title">
+        <Grid size={{ xs: 12, md: 7 }}>
+          <Typography
+            variant="body2"
+            paragraph
+            fontFamily="serif"
+            fontWeight="bold"
+            fontSize={16}
+            textAlign="center"
+            flexDirection="column"
+          >
+            Prazer, eu sou Wesley Amaro (mas pode me chamar de Wes). Tenho 28
+            anos, moro em São Paulo e sou apaixonado por dar vida a interfaces
+            incríveis. Sou estudante de ADS, dev Front-End por vocação,
+            Full-Stack em evolução e curioso profissional de T.I. há 6 anos.
+            <br />
+            <br />
+            Ao longo da minha trajetória profissional, atuei em áreas como
+            Desenvolvimento, Suporte Técnico, Assistência em T.I., Atendimento e
+            Liderança. Essa bagagem me deu visão ampla, foco em resultados e
+            jogo de cintura pra resolver qualquer pepino.
+            <br />
+            <br />
+            Atualmente finalizando minha graduação em Análise e Desenvolvimento
+            de Sistemas (ADS), com foco em Front-End e Full-Stack. Tenho
+            certificações em tecnologias web como HTML, CSS, JavaScript,
+            TypeScript, Node.js, React, Tailwind, Sass, Material UI e Figma.
+            <br />
+            <br />
+            Também estou expandindo minha base Full Stack com Python e SQL, e
+            faço do GitHub meu companheiro diário. De quebra, tenho noções de
+            linguagens como Java, C, C++ e C#, porque conhecer várias linguagens
+            é sempre poder de fogo extra.
+            <br />
+            <br />
+            São mais de 10 anos de experiência no mercado, 6 deles dedicados à
+            tecnologia. Nesse tempo, contribuí para o crescimento da Niu Fibra
+            (empresa júnior de Guarujá que hoje integra o grupo Alloha Fibra),
+            otimizando processos, criando soluções e melhorando a experiência
+            dos clientes com ferramentas como Excel, PowerPoint, Teams, 3CX,
+            AnyDesk, além de muito suporte a softwares e hardwares internos.
+            <br />
+            <br />
+            Meu objetivo? Seguir evoluindo como desenvolvedor, criando soluções
+            que impactem pessoas e negócios de verdade.
+          </Typography>
+        </Grid>
+      </AboutSection>
+      {/* </Element> */}
     </>
   );
 };
--- myportfolio-project/src/pages/Home/sections/About/AboutHeader.tsx
+++ myportfolio-project/src/pages/Home/sections/About/AboutHeader.tsx
@@ -1,13 +1,22 @@
 import { Box, Typography } from "@mui/material";
+import { Element } from "react-scroll";
 
 const AboutHeader: React.FC = () => {
   return (
     <>
-      <Box mb={4} textAlign="center">
-        <Typography variant="h2" fontStyle="italic" fontWeight="bold">
-          About Me 👨🏽‍💻
-        </Typography>
-      </Box>
+      <Element name="about">
+        <Box
+          mb={4}
+          textAlign="center"
+          id="about"
+          tabIndex={-1}
+          aria-labelledby="about-title"
+        >
+          <Typography variant="h2" fontStyle="italic" fontWeight="bold">
+            About Me 👨🏽‍💻
+          </Typography>
+        </Box>
+      </Element>
     </>
   );
 };
