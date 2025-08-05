27b420e6bd1b1d245a40eaa9e6eba941aba95867
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-08-05 15:27:02 -0300

    feat: create Projects.tsx

--- myportfolio-project/src/pages/Home/sections/Projetcs/Projects.tsx
+++ myportfolio-project/src/pages/Home/sections/Projetcs/Projects.tsx
@@ -1,7 +1,145 @@
+import {
+  Container,
+  styled,
+  Typography,
+  Grid,
+  CardContent,
+  Card,
+} from "@mui/material";
+
+const StyledAbout = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.secondary.contrastText,
+  color: theme.palette.primary.contrastText,
+  paddingBottom: "4rem",
+}));
+
+const StyledCardProjects = styled(Card)(({ theme }) => ({
+  display: "flex",
+  height: "100%",
+  padding: "0.6rem 0.6rem",
+  textAlign: "center",
+  marginBottom: "0.6rem",
+  flexDirection: "column",
+  backgroundColor: theme.palette.secondary.light,
+  color: theme.palette.primary.light,
+  "&:hover": {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.secondary.light,
+    transform: "scale(1.1)",
+    // borderRadius: "1rem",
+    zIndex: 10,
+    // transform: "perspective(1000px) rotateY(10deg) rotateX(10deg) scale(1.05)",
+    boxShadow: "0 20px 40px rgba(215, 216, 227, 0.3)",
+  },
+}));
+
+const project = [
+  {
+    title: "'My Family's App'",
+    description: "Responsive website in React + TS with animations and deploy",
+    tech: "Technology: React, TypeScript, Tailwind, Sass, Python, SQL",
+    repository: "https://github.com/pohawesleyp/my-family-app",
+    status: "Status: Under construction",
+  },
+  {
+    title: "'GitHub Portfólio'",
+    description:
+      "GitHub's responsive design, transforming your page into a Portfolio",
+    tech: "Technology: HTML, CSS, JavScript",
+    repository: "https://github.com/pohawesleyp/pohawesleyp",
+    status: "Finished",
+  },
+  {
+    title: "'To-Do List'",
+    description: "Responsive To-Do List in React + Tailwind",
+    tech: "Technology: React, JavaScript, CSS",
+    repository: "https://github.com/pohawesleyp/listaDeTarefas-react-tailwind",
+    status: "Finished",
+  },
+  {
+    title: "'Google Glas WebSite'",
+    description: "Google site in HTML, CSS and pure JavaScript",
+    tech: "Technology: HTML, CSS, JavaScript",
+    repository: "https://github.com/pohawesleyp/projeto-aula-google-glass",
+    status: "Finished",
+  },
+  {
+    title: "'My Portfolio'",
+    description: "Responsive website in React + TS with animations and deploy",
+    tech: "Technology: React, TypeScript, Material Ui",
+    repository: "https://github.com/pohawesleyp/myportfolio",
+    status: "Finished",
+  },
+];
+
 const Projects: React.FC = () => {
   return (
     <>
-      <h1>Em construção</h1>
+      <StyledAbout id="projects">
+        <Container maxWidth="lg">
+          <Typography
+            variant="h2"
+            textAlign={"center"}
+            fontStyle={"italic"}
+            fontWeight={"bolder"}
+            gutterBottom
+            paddingBottom={4}
+            paddingTop={4}
+          >
+            Projects 🚧
+          </Typography>
+
+          <Grid container spacing={2} justifyContent={"center"}>
+            {project.map((project, index) => (
+              <Grid
+                key={index}
+                alignItems={"stretch"}
+                size={{ xs: 12, sm: 6, md: 5 }}
+                justifyContent={"space-between"}
+              >
+                <StyledCardProjects>
+                  <CardContent>
+                    <Typography
+                      variant="h4"
+                      color="#ffffff"
+                      fontFamily={"-moz-initial"}
+                      fontWeight={"bold"}
+                    >
+                      {project.title}
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      fontFamily={"cursive"}
+                      fontWeight={"bold"}
+                    >
+                      {project.description}
+                    </Typography>
+                    <Typography variant="subtitle1" fontWeight={"bold"}>
+                      {project.tech}
+                    </Typography>
+                    <Typography variant="body2" fontWeight={"bold"}>
+                      <a
+                        href={project.repository}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{
+                          color: "inherit",
+                          textDecoration: "underline",
+                        }}
+                      >
+                        View Repository
+                      </a>
+                    </Typography>
+                    <Typography variant="h6" fontWeight={"bold"} color="white">
+                      {project.status}
+                    </Typography>
+                  </CardContent>
+                </StyledCardProjects>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </StyledAbout>
     </>
   );
 };
