406cc80b5b2b530ddf143f69309b4dc10a4a133b
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-08-06 11:05:31 -0300

    perf/style: change style and responsive cod

--- myportfolio-project/src/pages/Home/sections/About/AboutCourses.tsx
+++ myportfolio-project/src/pages/Home/sections/About/AboutCourses.tsx
@@ -1,100 +1,97 @@
-import { styled, Grid } from "@mui/material";
+import { styled, Grid, Card, Fade, CardContent } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import TypewriterBody from "../../../../components/Typewriter/TypewriterBody";
+import TypewriterCourse from "../../../../components/Typewriter/TypewriterCourse";
+import SchoolIcon from "@mui/icons-material/School";
+import { Description } from "@mui/icons-material";
 
 const AboutSection = styled(Box)(({ theme }) => ({
   marginBottom: theme.spacing(1),
   textAlign: "left",
+  padding: "0.6rem 0.5rem",
 }));
+
+const CourseCard = styled(Card)(({ theme }) => ({
+  backgroundColor: theme.palette.secondary.light,
+  color: theme.palette.primary.light,
+  transition: "transform 0.3s ease, box-shadow 0.3s ease",
+  cursor: "default",
+  padding: "0.6rem 0.5rem",
+  marginBottom: "0.6rem",
+  "&:hover": {
+    transform: "scale(1.05)",
+    backgroundColor: theme.palette.primary.light,
+    color: "white",
+    boxShadow: "0 20px 40px rgba(117, 194, 245, 0.3)",
+  },
+}));
+
+const courses = [
+  {
+    institution: "Ada Tech (Brazil)",
+    course: "Front-End Development with a Full Stack Approach",
+  },
+  {
+    institution: "Udemy (Brazil)",
+    course:
+      "Web Development Bootcamp Certificate (Java, Python, C, C++ and C#)",
+  },
+  { institution: "Santander Open Academy (Brazil)", course: "Python Basics" },
+  { institution: "Udemy (Brazil)", course: "Python Basics" },
+  {
+    institution: "Gustavo Guanabara (Brazil)",
+    course: "Web Development Bootcamp Certificate (HTML, CSS and JavaScript)",
+  },
+];
 
 const AboutCouses: React.FC = () => {
   return (
     <>
-      <Grid size={{ xs: 12, md: 5 }}>
-        <AboutSection>
-          <Typography
-            variant="h3"
-            gutterBottom
-            fontStyle="italic"
-            fontWeight="bold"
-            textAlign="center"
-          >
-            Courses with Certificates ⭐
-          </Typography>
+      <Grid container spacing={1} justifyContent={"center"}>
+        <Grid size={{ xs: 12, md: 7 }}>
+          <AboutSection>
+            <Typography>
+              <TypewriterCourse
+                text="Courses With Certificates ⭐"
+                delay={40}
+                variant="h3"
+                color="white"
+                fontWeight="bolder"
+              />
+            </Typography>
+          </AboutSection>
+        </Grid>
+      </Grid>
 
-          <TypewriterBody
-            text="        Institution: Ada Tech (Brazil)"
-            delay={40}
-            variant="body1"
-            color="white"
-          ></TypewriterBody>
-          <br />
-          <TypewriterBody
-            text="Course: Front-End Development with a Full Stack Approach"
-            delay={40}
-            variant="body1"
-            color="white"
-          ></TypewriterBody>
-          <br />
-          <TypewriterBody
-            text="Institution: Udemy (Brazil)"
-            delay={40}
-            variant="body1"
-            color="white"
-          ></TypewriterBody>
-          <br />
-          <TypewriterBody
-            text="Course: Web Development Bootcamp Certificate (Java, Python, C, C++
-              and C#)"
-            delay={40}
-            variant="body1"
-            color="white"
-          ></TypewriterBody>
-          <br />
-          <TypewriterBody
-            text=" Institution: Santander Open Academy (Brazil)"
-            delay={40}
-            variant="body1"
-            color="white"
-          ></TypewriterBody>
-          <br />
-          <TypewriterBody
-            text=" Course: Python Basics"
-            delay={40}
-            variant="body1"
-            color="white"
-          ></TypewriterBody>
-          <br />
-          <TypewriterBody
-            text="Institution: Udemy (Brazil)"
-            delay={40}
-            variant="body1"
-            color="white"
-          ></TypewriterBody>
-          <br />
-          <TypewriterBody
-            text="Course: Python Basics"
-            delay={40}
-            variant="body1"
-            color="white"
-          ></TypewriterBody>
-          <br />
-          <TypewriterBody
-            text="Institution: Gustavo Guanabara (Brazil)"
-            delay={40}
-            variant="body1"
-            color="white"
-          ></TypewriterBody>
-          <br />
-          <TypewriterBody
-            text="Course: Web Development Bootcamp Certificate (HTML, CSS and
-              JavaScript)"
-            delay={40}
-            variant="body1"
-            color="white"
-          ></TypewriterBody>
-        </AboutSection>
+      <Grid container spacing={3} justifyContent={"center"}>
+        {courses.map(({ institution, course }, index) => (
+          <Grid size={{ xs: 12, sm: 6, md: 6 }} key={index}>
+            <Fade in={true} style={{ transitionDelay: `${index * 300}ms` }}>
+              <CourseCard elevation={3}>
+                <CardContent>
+                  <Typography
+                    variant="subtitle1"
+                    fontWeight={"bold"}
+                    display={"flex"}
+                    alignItems={"center"}
+                    gap={1}
+                  >
+                    <SchoolIcon /> {institution}
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    mt={1}
+                    display={"flex"}
+                    alignItems={"center"}
+                    gap={1}
+                  >
+                    <Description /> {course}
+                  </Typography>
+                </CardContent>
+              </CourseCard>
+            </Fade>
+          </Grid>
+        ))}
       </Grid>
     </>
   );
