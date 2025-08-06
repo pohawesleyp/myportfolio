563c98df372ad99440fef8a552f80b9c628b2287
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-08-06 11:04:34 -0300

    feat/style: create Typewriter for Courses.tsx, whit new style

---
+++ myportfolio-project/src/components/Typewriter/TypewriterCourse.tsx
@@ -0,0 +1,46 @@
+import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import type { TypographyProps } from "@mui/material";
+
+interface TypewriterProps {
+  text: string;
+  delay?: number;
+  variant?: TypographyProps["variant"];
+  color?: string;
+  fontWeight?: string;
+}
+
+const TypewriterCourse: React.FC<TypewriterProps> = ({
+  text,
+  delay = 100,
+  variant = "body1",
+  color = "text.primary",
+  fontWeight = "",
+}) => {
+  const [currentText, setCurrentText] = useState("");
+  const [currentIndex, setCurrenteIndex] = useState(0);
+
+  useEffect(() => {
+    if (currentIndex < text.length) {
+      const timeoutId = setTimeout(() => {
+        setCurrentText((prevText) => prevText + text[currentIndex]);
+        setCurrenteIndex((prevIndex) => prevIndex + 1);
+      }, delay);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [currentIndex, delay, text]);
+
+  return (
+    <Typography
+      variant={variant}
+      color={color}
+      textAlign="center"
+      fontWeight={fontWeight}
+    >
+      {currentText}
+    </Typography>
+  );
+};
+
+export default TypewriterCourse;
