29c2e3376b5b8981d71c5822f6f3c86930e7f8b8
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-08-07 15:53:48 -0300

    feat: backend base

---
+++ myportfolio-backend/.venv/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, EmailStr
+
+app = FastAPI()
+
+# Habilitando o acesso do Front-End
+app.add._middleware (
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173/"], #Porta onde o React roda
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_header=["*"],
+)
+
+#Modelo do formulário de contato
+class ContactForm(BaseModel):
+    name: str
+    email: EmailStr
+    message: str
+
+
+
+# Endpoint POST /contact
+@app.post("/contact")
+def receive_contact(form: ContactForm):
+    print(f"\n📬 Nova mensagem de {form.name} ({form.email}):\n{form.message}\n")
+    return {"sucess": True, "message": "Mensagem recebida com sucesso!"}
