2cc4b57a70f30ad596f1157a358d431b9ac7a647
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-08-08 15:03:20 -0300

    feat: Created data model and connection to the database

--- myportfolio-backend/.venv/main.py
+++ myportfolio-backend/.venv/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, EmailStr
+from sqlalchemy import create_engine, Column, Integer, String, Text
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 
 app = FastAPI()
 
@@ -13,6 +16,23 @@ app.add_middleware (
     allow_headers=["*"],
 )
 
+DATABASE_URL = "sqlite:///./contacts.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100))
+    email = Column(String(100))
+    message = Column(Text)
+
+Base.metadata.create.all(bind=engine)
+
 #Modelo do formulário de contato
 class ContactForm(BaseModel):
     name: str
@@ -24,5 +44,11 @@ class ContactForm(BaseModel):
 # Endpoint POST /contact
 @app.post("/contact")
 def receive_contact(form: ContactForm):
+    db = SessionLocal()
+    contact = Contact(name=form.name, email=form.email, message=form.message)
+    db.add(contact)
+    db.commit()
+    db.refresh(contact)
+    db.close()
     print(f"\n📬 Nova mensagem de {form.name} ({form.email}):\n{form.message}\n")
     return {"success": True, "message": "Mensagem recebida com sucesso!"}
