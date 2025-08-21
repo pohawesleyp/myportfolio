b8bc76162afcf278c142b808a2d6b9a476109447
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-08-21 13:29:20 -0300

    feat: App rerouting to improve site performance and responsiveness, as well as boosting performance. / Mudança de rota em App para melhorar performance e responsividade do site. Além de subir o desempenho

--- myportfolio-project/src/App.tsx
+++ myportfolio-project/src/App.tsx
@@ -1,20 +1,47 @@
+import { Box, styled } from "@mui/material";
 import Footer from "./components/Footer/Footer";
 import ContactForm from "./components/Form/ContactForm";
 import NavBar from "./components/NavBar/NavBar";
-// import AboutCouses from "./pages/Home/sections/About/AboutCourses";
-// import AboutDescription from "./pages/Home/sections/About/AboutDescription";
-import AboutMain from "./pages/Home/sections/About/AboutMain";
+import AboutCards from "./pages/Home/sections/About/AboutCards";
+import AboutCouses from "./pages/Home/sections/About/AboutCourses";
+import AboutDescription from "./pages/Home/sections/About/AboutDescription";
+import AboutHeader from "./pages/Home/sections/About/AboutHeader";
 import AboutSkills from "./pages/Home/sections/About/AboutSkills";
 import Hero from "./pages/Home/sections/Hero/Hero";
 import Projects from "./pages/Home/sections/Projetcs/Projects";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const AboutContainer = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.secondary.contrastText,
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  color: theme.palette.primary.contrastText,
+  overflow: "hidden",
+  paddingBottom: "4rem", // adiciona espaço embaixo para o botão
+  [theme.breakpoints.down("sm")]: {
+    paddingBottom: "8rem", // mais espaço em telas pequenas
+  },
+}));
 
 const HomePage: React.FC = () => (
   <>
     <Hero />
-    <AboutMain />
+
+    <AboutContainer>
+      <AboutHeader />
+
+      <AboutCards />
+
+      <Suspense fallback={<div />}>
+        <AboutDescription />
+        <AboutCouses />
+      </Suspense>
+    </AboutContainer>
+
     <AboutSkills />
     <Projects />
   </>
